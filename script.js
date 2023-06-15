ff54911294a8c6782d8adc2e5a05a93b437ca270
gillend3 <136641449+gillend3@users.noreply.github.com>
2023-06-15 08:32:43 +0100

    Update script.js
    reworking to make easy/hard mode work

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-function executeMode(mode) {
-  if (mode === 'easy') {
-    document.getElementById("input-box").addEventListener("keydown", function(event) {
-      if (event.keyCode === 8 || event.keyCode === 46) {
-        event.preventDefault();
-  console.log("Easy Mode Executed");
-  }
-  });
-}
-  else if (mode === 'hard') {
-    var counter = 0;
-    var maxBackspaces = 10;
-    var inputBox = document.geElementById("input-box");
+var counter = 0;
+var maxBackspaces = 10;
+var inputBox = document.getElementById("input-box");
 
-  inputBox.addEventListener("keydown", function(event) {
-    if (event.key === "Backspace" || event.key === "Delete") {
-      counter ++;
-      if (counter > maxBackspaces) {
-        inputBox.innerHTML = "";
-        counter = 0
-      }
+inputBox.addEventListener("keydown", function(event) {
+  if (event.key === "Backspace" || event.key === "Delete") {
+    if (counter >= maxBackspaces) {
+      event.preventDefault(); // Prevent backspace or delete action
+      inputBox.innerHTML = ""; // Clear the input box
+      counter = 0; // Reset the counter
+    } else {
+      counter++;
     }
-  });
+  }
+});
+
+function executeMode(checked, mode) {
+  if (mode === 'easy') {
+    if (checked) {
+      inputBox.removeEventListener("keydown", handleHardMode);
+    } else {
+      inputBox.addEventListener("keydown", handleHardMode);
+    }
+  } else if (mode === 'hard') {
+    if (checked) {
+      inputBox.addEventListener("keydown", handleHardMode);
+    } else {
+      inputBox.removeEventListener("keydown", handleHardMode);
+    }
+  }
+}
+
+function handleHardMode(event) {
+  if (event.key === "Backspace" || event.key === "Delete") {
+    if (counter >= maxBackspaces) {
+      event.preventDefault(); // Prevent backspace or delete action
+      inputBox.innerHTML = ""; // Clear the input box
+      counter = 0; // Reset the counter
+    } else {
+      counter++;
+    }
   }
 }
