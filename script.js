6c40f9df6aa8837aeeca49581d7bf6da29254d7c
gillend3 <136641449+gillend3@users.noreply.github.com>
2023-06-15 08:05:16 +0100

    Update script.js
    updated to add different functions for hard and easy mode

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-document.getElementById("input-box").addEventListener("keydown", function(event) {
+function executeEasyMode() {
+  document.getElementById("input-box").addEventListener("keydown", function(event) {
   if (event.keyCode === 8 || event.keyCode === 46) {
     event.preventDefault();
+  console.log("Easy Mode Executed");
   }
-});
+  });
+}
+
+function executedHardMode() {
+  var counter = 0;
+  var maxBackspaces = 10;
+  var inputBox = document.getElementById("input-box");
+
+  inputbox.addEventListener("keydown", function(event) {
+    if (event.key === "Backspace" || event.key = "Delete") {
+      counter++;
+      if (counter >= maxBackspaces) {
+        inputBox.innerHTML = "";
+        counter = 0;
+      }
+    }
+  });
+}
