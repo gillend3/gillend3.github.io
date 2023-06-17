da86746873b2c3ca906c95d951b1da8e48707072
gillend3 <136641449+gillend3@users.noreply.github.com>
2023-06-17 12:00:31 +0100

    Update script.js
    going back to simpler times - user simply can't delete

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-var counter = 0;
-var maxBackspaces = 10;
-var inputBox = document.getElementById("input-box");
-var hardModeEnabled = false;
-
-inputBox.addEventListener("keydown", function(event) {
-  if (hardModeEnabled && (event.key === "Backspace" || event.key === "Delete")) {
-    if (counter >= maxBackspaces) {
-      event.preventDefault(); // Prevent backspace or delete action
-      inputBox.innerHTML = ""; // Clear the input box
-      counter = 0; // Reset the counter
-    } else {
-      counter++;
-    }
-  } else if (!hardModeEnabled) {
-    event.preventDefault(); // Prevent backspace or delete action in easy mode
+document.getElementById("input-box").addEventListener("keydown", function(event) {
+  if (event.keyCode === 8 || event.keyCode === 46) {
+    event.preventDefault();
   }
 });
-
-function executeMode(mode) {
-  if (mode === 'easy') {
-    hardModeEnabled = false;
-  } else if (mode === 'hard') {
-    hardModeEnabled = true;
-  }
-}
